974206a4b511724783c2ff46afef17d89b8ce3ea
Will Hegedus <whegedus@linode.com>
2021-12-01 08:38:25 -0500

    day1

--- solutions/day1.py
+++ solutions/day1.py
@@ -3,7 +3,17 @@ from solutions.Puzzle import Puzzle
 
 class Day1(Puzzle):
     def part1(self) -> int:
-        return 12
+        increases = 0
+
+        for n in range(1, len(self.data)):
+            if self.data[n] > self.data[n - 1]:
+                increases += 1
+
+        return increases
 
     def part2(self) -> int:
-        return 2322
+        # 171 + 154 + 155 + 170
+        # 171 + 154 + 155 < 154 + 155 + 170
+        # False, because a (171) is greater than d (170)
+        # Only need to compare the number being swapped from the sliding scale
+        return sum(y > x for x, y in zip(self.data, self.data[3:]))
