f3de0cd8c745c5ab85a5da770434538c7ab1c8ea
Will Hegedus <whegedus@linode.com>
2021-12-06 07:09:34 -0500

    add flag for test input

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ My solutions to AoC 2021 puzzles
 
 ## Usage 
 `python3 main.py day1`
+
+Use test input:
+`python3 main.py day5 --test`
--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from solutions.Puzzle import Puzzle
 from importlib import import_module
 
 
-def dynamicimport(day: str):
+def dynamicimport(day: str, test=False):
     mod = import_module(f"solutions.{day}")
     solution = getattr(mod, day.title())
-    return solution(day)
+    if test:
+        return solution(day+"test")
+    else:
+        return solution(day)
 
 
 parser = ArgumentParser()
 parser.add_argument("day", help="which day to run")
+parser.add_argument("--test", action="store_true", help="Use {day}test.txt for input")
 args = parser.parse_args()
 
-day: Puzzle = dynamicimport(args.day)
+day: Puzzle = dynamicimport(args.day, args.test)
 
 day.solve()
